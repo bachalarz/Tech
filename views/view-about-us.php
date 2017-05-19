99065859d055085d52b79f114d736d39c5ca4542
Magnus Bachalarz <magnus@bachalarz.dk>
2017-05-19 10:27:29 +0200

    about us

--- views/view-about-us.php
+++ views/view-about-us.php
@@ -4,6 +4,26 @@
 	
 	<div class="container">
 		<h1>About Us</h1>
+		
+		<div class="container-inner-wrapper">
+			<div class="container-inner-wrapper-element">
+				<h2>We live tech</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras imperdiet orci vel luctus pulvinar. Donec eget mi at felis dignissim pretium. Suspendisse fermentum dui in est finibus imperdiet. Donec vitae sodales magna. Sed fringilla in ante id elementum. Nam at laoreet urna, eget faucibus dui. Duis et lorem quam. Etiam at turpis mi. Nulla nec nibh eu tortor vestibulum suscipit. Donec ultricies, nulla quis semper venenatis, ex elit rhoncus justo, vel accumsan turpis ante sed urna.</p>
+				<button>See partners</button>
+			</div>
+			<div class="container-inner-wrapper-element">
+				<img src="img/placeholder/seminar.jpg">
+			</div>
+		</div>
+	
+
+
+
+
+
+
+
+
 	</div>
 
 	<?php
