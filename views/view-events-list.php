dc54088c99a59322955d647d04fcb470d5e0a6fe
Nora Mbuywana <nmsifunisohellesoe@hotmail.com>
2017-05-22 19:58:25 +0200

    j

--- views/view-events-list.php
+++ views/view-events-list.php
@@ -17,23 +17,229 @@
 
 					<div class="btn-search-filter"></div>
 				</div>
+			</div>
 
-				<div id="ctn-categories">
-					<div class="btn-category" id="btn-mobile" data-show-category=""></div>
-					<div class="btn-category" id="btn-web" data-show-category=""></div>
-					<div class="btn-category" id="btn-design" data-show-category=""></div>
-					<div class="btn-category" id="btn-games" data-show-category=""></div>
-					<div class="btn-category" id="btn-some" data-show-category=""></div>
-					<div class="btn-category" id="btn-more" data-show-category=""></div>
+			<div id="ctn-categories">
+				<div class="btn-category" id="btn-mobile" data-show-category="">
+					<?php require_once 'icons/mobile.svg';?>
 				</div>
 
+				<div class="btn-category" id="btn-web" data-show-category="">
+					<?php require_once 'icons/web.svg';?>
+				</div>
+
+				<div class="btn-category" id="btn-design" data-show-category="">
+					<?php require_once 'icons/design.svg';?>
+				</div>
+
+				<div class="btn-category" id="btn-games" data-show-category="">
+					<?php require_once 'icons/gaming.svg';?>
+				</div>
+
+				<div class="btn-category" id="btn-some" data-show-category="">
+					<?php require_once 'icons/some.svg';?>
+				</div>
+					
+				<div class="btn-category" id="btn-more" data-show-category="">
+					<?php require_once 'icons/more.svg';?>	
+				</div>
 			</div>
+
 
 			<div class="ctn-events">
-				<div class="event"></div>
+				<div class="noslide-container">
+
+					<div class="noslide">
+		  				<div class="img-slide-container img-slide-container-1">
+		            		<div class="filter filter-1">
+		            			<div class="info">
+		            				<h3>awesome event</h3>
+		            				<h4>copenhagen - 2. may - 100 DKKR</h4>
+		            			</div>
+		            		</div>
+		          		</div>
+		  				<div>
+		  					<p>
+		  						Lorem Ipsum er ganske enkelt fyldtekst fra print- og typografiindustrien. Lorem Ipsum har været standard fyldtekst siden 1500-tallet, hvor en ukendt trykker sammensatte en tilfældig spalte for at trykke en bog til sammenligning af forskellige skrifttyper.
+		  					</p>
+		  					<div class="noslide-actions">
+		  						<div class="noslide-action link" data-go-to="event" style="border-right: 1px solid grey; height: 100%;">READ MORE</div>
+		  						<div class="noslide-action link" data-go-to="payment">BOOK</div>
+		  					</div>
+		  				</div>
+		  			</div>
+
+		  			<div class="noslide">
+		  				<div class="img-slide-container img-slide-container-2">
+		            		<div class="filter filter-2">
+		            			<div class="info">
+		            				<h3>awesome event</h3>
+		            				<h4>copenhagen - 2. may - 100 DKKR</h4>
+		            			</div>
+		            		</div>
+		          		</div>
+		  				<div>
+		  					<p>
+		  						Lorem Ipsum er ganske enkelt fyldtekst fra print- og typografiindustrien. Lorem Ipsum har været standard fyldtekst siden 1500-tallet, hvor en ukendt trykker sammensatte en tilfældig spalte for at trykke en bog til sammenligning af forskellige skrifttyper.
+		  					</p>
+		  					<div class="noslide-actions">
+		  						<div class="noslide-action link" data-go-to="event" style="border-right: 1px solid grey; height: 100%;">READ MORE</div>
+		  						<div class="noslide-action link" data-go-to="payment">BOOK</div>
+		  					</div>
+		  				</div>
+		  			</div>
+
+
+
+		  			<div class="noslide">
+		  				<div class="img-slide-container img-slide-container-3">
+		            		<div class="filter filter-3">
+		            			<div class="info">
+		            				<h3>awesome event</h3>
+		            				<h4>copenhagen - 2. may - 100 DKKR</h4>
+		            			</div>
+		            		</div>
+		          		</div>
+		  				<div>
+		  					<p>
+		  						Lorem Ipsum er ganske enkelt fyldtekst fra print- og typografiindustrien. Lorem Ipsum har været standard fyldtekst siden 1500-tallet, hvor en ukendt trykker sammensatte en tilfældig spalte for at trykke en bog til sammenligning af forskellige skrifttyper.
+		  					</p>
+		  					<div class="noslide-actions">
+		  						<div class="noslide-action link" data-go-to="event" style="border-right: 1px solid grey; height: 100%;">READ MORE</div>
+		  						<div class="noslide-action link" data-go-to="payment">BOOK</div>
+		  					</div>
+		  				</div>
+		  			</div>
+
+		  			<div class="noslide">
+		  				<div class="img-slide-container img-slide-container-1">
+		            		<div class="filter filter-1">
+		            			<div class="info">
+		            				<h3>awesome event</h3>
+		            				<h4>copenhagen - 2. may - 100 DKKR</h4>
+		            			</div>
+		            		</div>
+		          		</div>
+		  				<div>
+		  					<p>
+		  						Lorem Ipsum er ganske enkelt fyldtekst fra print- og typografiindustrien. Lorem Ipsum har været standard fyldtekst siden 1500-tallet, hvor en ukendt trykker sammensatte en tilfældig spalte for at trykke en bog til sammenligning af forskellige skrifttyper.
+		  					</p>
+		  					<div class="noslide-actions">
+		  						<div class="noslide-action link" data-go-to="event" style="border-right: 1px solid grey; height: 100%;">READ MORE</div>
+		  						<div class="noslide-action link" data-go-to="payment">BOOK</div>
+		  					</div>
+		  				</div>
+		  			</div>
+
+		  			<div class="noslide">
+		  				<div class="img-slide-container img-slide-container-2">
+		            		<div class="filter filter-2">
+		            			<div class="info">
+		            				<h3>awesome event</h3>
+		            				<h4>copenhagen - 2. may - 100 DKKR</h4>
+		            			</div>
+		            		</div>
+		          		</div>
+		  				<div>
+		  					<p>
+		  						Lorem Ipsum er ganske enkelt fyldtekst fra print- og typografiindustrien. Lorem Ipsum har været standard fyldtekst siden 1500-tallet, hvor en ukendt trykker sammensatte en tilfældig spalte for at trykke en bog til sammenligning af forskellige skrifttyper.
+		  					</p>
+		  					<div class="noslide-actions">
+		  						<div class="noslide-action link" data-go-to="event" style="border-right: 1px solid grey; height: 100%;">READ MORE</div>
+		  						<div class="noslide-action link" data-go-to="payment">BOOK</div>
+		  					</div>
+		  				</div>
+		  			</div>
+
+
+
+		  			<div class="noslide">
+		  				<div class="img-slide-container img-slide-container-3">
+		            		<div class="filter filter-3">
+		            			<div class="info">
+		            				<h3>awesome event</h3>
+		            				<h4>copenhagen - 2. may - 100 DKKR</h4>
+		            			</div>
+		            		</div>
+		          		</div>
+		  				<div>
+		  					<p>
+		  						Lorem Ipsum er ganske enkelt fyldtekst fra print- og typografiindustrien. Lorem Ipsum har været standard fyldtekst siden 1500-tallet, hvor en ukendt trykker sammensatte en tilfældig spalte for at trykke en bog til sammenligning af forskellige skrifttyper.
+		  					</p>
+		  					<div class="noslide-actions">
+		  						<div class="noslide-action link" data-go-to="event" style="border-right: 1px solid grey; height: 100%;">READ MORE</div>
+		  						<div class="noslide-action link" data-go-to="payment">BOOK</div>
+		  					</div>
+		  				</div>
+		  			</div>
+
+
+		  			<div class="noslide">
+		  				<div class="img-slide-container img-slide-container-1">
+		            		<div class="filter filter-1">
+		            			<div class="info">
+		            				<h3>awesome event</h3>
+		            				<h4>copenhagen - 2. may - 100 DKKR</h4>
+		            			</div>
+		            		</div>
+		          		</div>
+		  				<div>
+		  					<p>
+		  						Lorem Ipsum er ganske enkelt fyldtekst fra print- og typografiindustrien. Lorem Ipsum har været standard fyldtekst siden 1500-tallet, hvor en ukendt trykker sammensatte en tilfældig spalte for at trykke en bog til sammenligning af forskellige skrifttyper.
+		  					</p>
+		  					<div class="noslide-actions">
+		  						<div class="noslide-action link" data-go-to="event" style="border-right: 1px solid grey; height: 100%;">READ MORE</div>
+		  						<div class="noslide-action link" data-go-to="payment">BOOK</div>
+		  					</div>
+		  				</div>
+		  			</div>
+
+		  			<div class="noslide">
+		  				<div class="img-slide-container img-slide-container-2">
+		            		<div class="filter filter-2">
+		            			<div class="info">
+		            				<h3>awesome event</h3>
+		            				<h4>copenhagen - 2. may - 100 DKKR</h4>
+		            			</div>
+		            		</div>
+		          		</div>
+		  				<div>
+		  					<p>
+		  						Lorem Ipsum er ganske enkelt fyldtekst fra print- og typografiindustrien. Lorem Ipsum har været standard fyldtekst siden 1500-tallet, hvor en ukendt trykker sammensatte en tilfældig spalte for at trykke en bog til sammenligning af forskellige skrifttyper.
+		  					</p>
+		  					<div class="noslide-actions">
+		  						<div class="noslide-action link" data-go-to="event" style="border-right: 1px solid grey; height: 100%;">READ MORE</div>
+		  						<div class="noslide-action link" data-go-to="payment">BOOK</div>
+		  					</div>
+		  				</div>
+		  			</div>
+
+
+
+		  			<div class="noslide">
+		  				<div class="img-slide-container img-slide-container-3">
+		            		<div class="filter filter-3">
+		            			<div class="info">
+		            				<h3>awesome event</h3>
+		            				<h4>copenhagen - 2. may - 100 DKKR</h4>
+		            			</div>
+		            		</div>
+		          		</div>
+		  				<div>
+		  					<p>
+		  						Lorem Ipsum er ganske enkelt fyldtekst fra print- og typografiindustrien. Lorem Ipsum har været standard fyldtekst siden 1500-tallet, hvor en ukendt trykker sammensatte en tilfældig spalte for at trykke en bog til sammenligning af forskellige skrifttyper.
+		  					</p>
+		  					<div class="noslide-actions">
+		  						<div class="noslide-action link" data-go-to="event" style="border-right: 1px solid grey; height: 100%;">READ MORE</div>
+		  						<div class="noslide-action link" data-go-to="payment">BOOK</div>
+		  					</div>
+		  				</div>
+		  			</div>
+
+
+
 
 			</div>
-
 		</div>
 
 
