1bc95eb86d7d2e34149d47f2cb4b183cf26a1048
Magnus Bachalarz <magnus@bachalarz.dk>
2017-05-22 15:13:07 +0200

    main js - add frontpage

--- js/js-main.js
+++ js/js-main.js
@@ -2,7 +2,7 @@
 //		Main JS
 /**********************************/
 
-var startpage = "login"; // Startside kan indsættes her
+var startpage = "frontpage"; // Startside kan indsættes her
 
 
 /**********************************/
